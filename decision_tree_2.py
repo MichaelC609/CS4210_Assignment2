a9428646366decbdd21e593dbba8b0a64546718b
MichaelC609 <mjcastillo@cpp.edu>
2025-03-04 22:46:58 -0800

    completed question 2

--- decision_tree_2.py
+++ decision_tree_2.py
@@ -6,14 +6,30 @@
 # TIME SPENT: how long it took you to complete the assignment
 #-----------------------------------------------------------*/
 
-#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
-# dictionaries, lists, and arrays
+# IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas.
+# You have to work here only with standard dictionaries, lists, and arrays.
 
-#Importing some Python libraries
+# Importing some Python libraries
 from sklearn import tree
 import csv
 
 dataSets = ['contact_lens_training_1.csv', 'contact_lens_training_2.csv', 'contact_lens_training_3.csv']
+
+# Mapping categorical values to numerical values
+categoryMap = {
+    "Young": 1, "Prepresbyopic": 2, "Presbyopic": 3,
+    "Myope": 1, "Hypermetrope": 2,
+    "Yes": 1, "No": 2,
+    "Reduced": 1, "Normal": 2
+}
+
+# Read test data
+dbTest = []
+with open('contact_lens_test.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for i, row in enumerate(reader):
+        if i > 0:  # Skipping the header
+            dbTest.append(row)
 
 for ds in dataSets:
 
@@ -21,46 +37,54 @@ for ds in dataSets:
     X = []
     Y = []
 
-    #Reading the training data in a csv file
+    # Reading the training data from a CSV file
     with open(ds, 'r') as csvfile:
-         reader = csv.reader(csvfile)
-         for i, row in enumerate(reader):
-             if i > 0: #skipping the header
-                dbTraining.append (row)
+        reader = csv.reader(csvfile)
+        for i, row in enumerate(reader):
+            if i > 0:  # Skipping the header
+                dbTraining.append(row)
 
-    #Transform the original categorical training features to numbers and add to the 4D array X.
-    #For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3, X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
-    #--> add your Python code here
+    # Transform the original categorical training features to numbers and add to the 4D array X.
+    for row in dbTraining:
+        X.append([categoryMap[row[0]], categoryMap[row[1]], categoryMap[row[2]], categoryMap[row[3]]])
 
-    #Transform the original categorical training classes to numbers and add to the vector Y.
-    #For instance Yes = 1 and No = 2, Y = [1, 1, 2, 2, ...]
-    #--> add your Python code here
+    # Transform the original categorical training classes to numbers and add to the vector Y.
+    for row in dbTraining:
+        Y.append(categoryMap[row[4]])
 
-    #Loop your training and test tasks 10 times here
-    for i in range (10):
+    total_accuracy = 0
 
-       #Fitting the decision tree to the data setting max_depth=3
-       clf = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth=5)
-       clf = clf.fit(X, Y)
+    # Loop training and testing tasks 10 times
+    for i in range(10):
 
-       #Read the test data and add this data to dbTest
-       #--> add your Python code here
+        # Fitting the decision tree to the data, setting max_depth=5
+        clf = tree.DecisionTreeClassifier(criterion='entropy', max_depth=5)
+        clf = clf.fit(X, Y)
 
-       for data in dbTest:
-           #Transform the features of the test instances to numbers following the same strategy done during training,
-           #and then use the decision tree to make the class prediction. For instance: class_predicted = clf.predict([[3, 1, 2, 1]])[0]
-           #where [0] is used to get an integer as the predicted class label so that you can compare it with the true label
-           #--> add your Python code here
+        correct_predictions = 0
+        total_predictions = 0
 
-           #Compare the prediction with the true label (located at data[4]) of the test instance to start calculating the accuracy.
-           #--> add your Python code here
+        for data in dbTest:
+            # Transform test features using the same numerical mapping
+            test_instance = [categoryMap[data[0]], categoryMap[data[1]], categoryMap[data[2]], categoryMap[data[3]]]
+            class_predicted = clf.predict([test_instance])[0]
 
-    #Find the average of this model during the 10 runs (training and test set)
-    #--> add your Python code here
+            # Compare prediction with true label
+            true_label = categoryMap[data[4]]
+            if class_predicted == true_label:
+                correct_predictions += 1
 
-    #Print the average accuracy of this model during the 10 runs (training and test set).
-    #Your output should be something like that: final accuracy when training on contact_lens_training_1.csv: 0.2
-    #--> add your Python code here
+            total_predictions += 1
+
+        # Calculate accuracy for this run
+        accuracy = correct_predictions / total_predictions
+        total_accuracy += accuracy
+
+    # Find the average accuracy for the 10 runs
+    avg_accuracy = total_accuracy / 10
+
+    # Print the average accuracy of this model
+    print(f'Final accuracy when training on {ds}: {avg_accuracy:.2f}')
 
 
 
