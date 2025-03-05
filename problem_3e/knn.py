2da30a4fe8326b702f64fff06b050883a21ed58d
MichaelC609 <mjcastillo@cpp.edu>
2025-03-04 22:58:34 -0800

    complted problem 3e

--- problem_3e/knn.py
+++ problem_3e/knn.py
@@ -1,56 +1,58 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Michael Castillo
+# FILENAME: knn.py
+# SPECIFICATION: comupte the LOO-CV error rate for a 1NN classifier
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 20 minutes
 #-----------------------------------------------------------*/
 
-#IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
+# IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas.
+# You have to work here only with standard vectors and arrays.
 
-#Importing some Python libraries
+# Importing some Python libraries
 from sklearn.neighbors import KNeighborsClassifier
 import csv
 
 db = []
 
-#Reading the data in a csv file
-with open('binary_points.csv', 'r') as csvfile:
-  reader = csv.reader(csvfile)
-  for i, row in enumerate(reader):
-      if i > 0: #skipping the header
-         db.append (row)
+# Reading the data in a csv file
+with open('email_classification.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader)  # Skipping the header
+    for row in reader:
+        db.append(row)
 
-#Loop your data to allow each instance to be your test set
-for i in db:
+# Initialize error count
+error_count = 0
 
-    #Add the training features to the 20D array X removing the instance that will be used for testing in this iteration.
-    #For instance, X = [[1, 2, 3, 4, 5, ..., 20]].
-    #Convert each feature value to float to avoid warning messages
-    #--> add your Python code here
+total_instances = len(db)
 
-    #Transform the original training classes to numbers and add them to the vector Y.
-    #Do not forget to remove the instance that will be used for testing in this iteration.
-    #For instance, Y = [1, 2, ,...].
-    #Convert each feature value to float to avoid warning messages
-    #--> add your Python code here
+# Loop through each instance as a test set
+for i in range(total_instances):
+    # Add the training features to the 20D array X, removing the test instance
+    X = [list(map(float, row[:-1])) for j, row in enumerate(db) if j != i]
+    
+    # Transform the original training classes to numbers and add them to Y
+    Y = [1 if row[-1] == "spam" else 0 for j, row in enumerate(db) if j != i]
+    
+    # Store the test sample of this iteration
+    test_sample = list(map(float, db[i][:-1]))
+    true_label = 1 if db[i][-1] == "spam" else 0
 
-    #Store the test sample of this iteration in the vector testSample
-    #--> add your Python code here
-
-    #Fitting the knn to the data
+    # Fitting the 1NN classifier to the data
     clf = KNeighborsClassifier(n_neighbors=1, p=2)
-    clf = clf.fit(X, Y)
+    clf.fit(X, Y)
 
-    #Use your test sample in this iteration to make the class prediction. For instance:
-    #class_predicted = clf.predict([[1, 2, 3, 4, 5, ..., 20]])[0]
-    #--> add your Python code here
+    # Use the test sample in this iteration to make the class prediction
+    class_predicted = clf.predict([test_sample])[0]
 
-    #Compare the prediction with the true label of the test instance to start calculating the error rate.
-    #--> add your Python code here
+    # Compare the prediction with the true label
+    if class_predicted != true_label:
+        error_count += 1
 
-#Print the error rate
-#--> add your Python code here
+# Compute and print the error rate
+error_rate = error_count / total_instances
+print("LOO-CV Error Rate:", error_rate)
 
 
 
